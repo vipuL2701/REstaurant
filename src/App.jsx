c9bda64dc5a84c0595742a66cc299a1f645b6190
vipuL2701 <142662414+vipuL2701@users.noreply.github.com>
2023-10-03 16:07:36 +0530

    1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+//  import Bform from './components/Bform'
+import Login from './components/Login';
+
+function App() {
+ return (
+  <>
+   <Login />
+   </>
+  )
+}
+
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+import "./Login.css"
+import email_icon from "./assets/email.png"
+import user_icon from "./assets/person.png"
+import password_icon from "./assets/password.png"
+
+function Login() {
+  const [action,setAction] = useState ("Sign Up")
+
+  return (
+    <div className="container">
+        <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+        </div>
+
+        <div className="inputs">
+          {action==="Login"?<div></div>: <div className="input">
+          <img  src={user_icon} alt="" />
+         <input type="text" placeholder="Name" />
+      </div>
+      }
+       
+      <div className="input">
+        <img src={email_icon} alt="" />
+        <input type="email" placeholder="Email Id" />
+      </div>
+
+      <div className="input">
+        <img src={password_icon} alt="" />
+        <input type="password" placeholder="Password"/>
+      </div>
+     </div>
+       {action==="Sign Up"?<div></div>: 
+       <div className="forgot-password">Lost Password?<span>Click here</span></div>}
+    
+    <div className="submit-container">
+     <div className={action==="Login"?"submit gray" :"submit"}
+     onClick={() =>(setAction("Sign Up"))}>Sign Up</div>
+    <div className={action==="Sign Up"?"submit gray" :"submit"}
+     onClick={() =>(setAction("Login"))}>Login</div>   
+    </div>
+    </div>
+  )
+}
+
+export default Login
